a572fa0fb2484da6baf553c2c6b4a27961a13316
arnav-mani09 <arnavmani01@gmail.com>
2026-02-05 19:51:22 -0800

    fixing email issue

--- server/src/app.ts
+++ server/src/app.ts
@@ -22,10 +22,13 @@ const allowedOrigins = (
 const defaultLocalOrigins = [
   "http://127.0.0.1:5500",
   "http://127.0.0.1:5501",
+  "http://127.0.0.1:5173",
   "http://localhost:5500",
   "http://localhost:5501",
+  "http://localhost:5173",
   "http://[::]:5500",
   "http://[::]:5501",
+  "http://[::]:5173",
 ];
 const allowedOriginSet = new Set([...allowedOrigins, ...defaultLocalOrigins]);
 
